c0253ddc06372664d9ccac9b5a0af7fbc07aa2e1
steelerx155 <steelerx155@gmail.com>
2020-07-24 14:45:22 -0400

    Done.

--- .history/js/createStore_20200724142416.js
+++ .history/js/createStore_20200724142416.js
@@ -20,9 +20,7 @@ function render() {
   let container = document.getElementById('container');
   container.textContent = store.getState().count;
 };
- 
-let store = createStore();
-store.dispatch({ type: '@@INIT' });
+
 let button = document.getElementById('button');
  
 button.addEventListener('click', () => {
